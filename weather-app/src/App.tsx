b7d54eac880f6a478d57a394a211596fba284338
Kaveenxz <kaveenhansithx@gmail.com>
2024-02-25 10:11:27 +0530

    get data from api

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,46 +1,63 @@
-
-import './App.css'
-import './Other.css'
+import './App.css';
 import sunImage from './assets/sun (1).png';
-import searchimg from './assets/search.png'
-
+import searchimg from './assets/search.png';
+import React, { useState } from 'react';
 
 function App() {
-  
+  const [searchVal, setSearchVal] = useState('');
+
+  const fetchData = () => {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=e9efbd0df6294e5c9fb94321233012&q=${searchVal}`)
+      .then(response => response.json())
+      .then(data => {
+       
+        console.log(data);
+        
+
+       
+      })
+      .catch(error => console.log("error", error));
+  };
+
   return (
     <div className='container row full'>
-     <div className="col-8">
-     <div className='l-side'>
-            <h4 className='title'>The Weather</h4>
+      <div className="col-8">
+        <div className='l-side'>
+          <h4 className='title'>The Weather</h4>
 
-            <div className="content">
-                <h4 id='temp'>Temp:</h4>
-                <div className="countr">
-                    <h4 id='country'>Country:</h4>
-                    <h4 id='date'>Date</h4>
-                </div>
-                <div className="img">
-                    <img src={sunImage} alt="img" id='weather-img'/>
-                    <h4 id='weather'>Weather:</h4>
-                </div>
+          <div className="content">
+            <h4 id='temp'>Temp:</h4>
+            <div className="countr">
+              <h4 id='country'>Country:</h4>
+              <h4 id='date'>Date</h4>
             </div>
+            <div className="img">
+              <img src={sunImage} alt="img" id='weather-img' />
+              <h4 id='weather'>Weather:</h4>
+            </div>
+          </div>
         </div>
-     </div>
-     <div className="col-4">
-     <div className='r-side'>
-           <div className="search">
-           <input type="search" placeholder='Enter Location' />
-            <img src={searchimg} alt="" />
-           </div>
+      </div>
+      <div className="col-4">
+        <div className='r-side'>
+          <div className="search">
+            <input
+              type="search"
+              placeholder='Enter Location'
+              id='searchVal'
+              value={searchVal}
+              onChange={(e) => setSearchVal(e.target.value)}
+            />
+            <img src={searchimg} alt="" onClick={fetchData} />
+          </div>
 
-           <div className="weather-details">
+          <div className="weather-details">
             <h4>Weather Details</h4>
-           </div>
-
+          </div>
         </div>
-     </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
